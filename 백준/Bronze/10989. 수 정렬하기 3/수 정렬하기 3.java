75394ac0d6aaf8e0a1e38d76833ef9f4049f3954
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 20:05:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2612 ms, Memory: 345448 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		int[] numbers = new int[size];
+
+		for(int i=0; i<size; i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(numbers);
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=0; i<size; i++) {
+			bw.write(numbers[i] + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
