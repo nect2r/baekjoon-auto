3078fdaec3615d994cc0b52c63b5f090f7c7fa3e
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 20:07:29 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = bf.readLine().split(" ");
+        long a = Long.parseLong(strs[0]);
+        long b = Long.parseLong(strs[1]);
+        long c = Long.parseLong(strs[2]);
+		System.out.println(a+b+c);
+	}
+}
