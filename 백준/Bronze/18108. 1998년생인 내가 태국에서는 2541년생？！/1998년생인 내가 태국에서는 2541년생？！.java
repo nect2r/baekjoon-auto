59f1249f0d24d5a6c6c0e1daf75e687b04228511
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 20:14:14 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+             Scanner input = new Scanner(System.in);
+             int year = input.nextInt();
+             System.out.println(year - 543);
+    }
+}
