a8c609154c9cd2b2b2b821133b45bbcd32fbe177
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 20:06:44 +0900

    [Bronze V] Title: A+B - 7, Time: 268 ms, Memory: 18724 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+    	Scanner scanner = new Scanner(System.in);
+        Integer T = scanner.nextInt();
+        
+        for(int i=1; i<=T; i++) {
+        	Integer A = scanner.nextInt();
+        	Integer B = scanner.nextInt();
+        	
+        	if((0 < A && A < 10) && (0 < B && B < 10)) {
+        		System.out.println("Case #" + i + ": " + (A + B));
+        	}else {
+        		System.out.println("0보다 커야하며 10보다 작아야합니다.");
+        	}
+        }
+        
+        scanner.close();
+    }
+}
