48fc38ce346b153ee175cdabe2a9bba71bf227f0
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 19:22:59 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 18008 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+             Scanner input = new Scanner(System.in);
+             double num1 = input.nextDouble();
+             double num2 = input.nextDouble();
+             System.out.println(num1 / num2);
+    }
+}
