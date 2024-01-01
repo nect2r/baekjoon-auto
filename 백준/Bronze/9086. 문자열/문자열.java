8017ae2cf9ead70a974756fb076693b56f27eaef
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 19:55:35 +0900

    [Bronze V] Title: 문자열, Time: 144 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+
+		for(int i=0; i<size; i++) {
+			String str = br.readLine();
+			System.out.println((""+str.charAt(0)) + (""+str.charAt(str.length()-1)));
+		}
+	}
+}
