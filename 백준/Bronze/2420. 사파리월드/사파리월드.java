c8bc5f0e614a6d801df77cdb2254724dc834e826
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 19:37:22 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = bf.readLine().split(" ");
+        long a = Long.parseLong(strs[0]);
+        long b = Long.parseLong(strs[1]);
+		System.out.println(Math.abs(a - b));
+	}
+}
