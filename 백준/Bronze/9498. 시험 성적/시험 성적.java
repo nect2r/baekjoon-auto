b1503df21b5077b222965a131194d1de6c46029a
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 19:55:58 +0900

    [Bronze V] Title: 시험 성적, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        scanner.close();
+        if(90 <= A && A <= 100){
+            System.out.println("A");
+        }else if(80 <= A && A <= 89){
+            System.out.println("B");
+        }else if(70 <= A && A <= 79){
+            System.out.println("C");
+        }else if(60 <= A && A <= 69){
+            System.out.println("D");
+        }else{
+            System.out.println("F");
+        }
+    }
+}
