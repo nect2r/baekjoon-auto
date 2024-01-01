a32a2e81c6c304391eb43e3ba065b6353611454d
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-01-01 20:00:05 +0900

    [Bronze III] Title: 공 넣기, Time: 148 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,40 @@
+/*
+ * @since 2023-02-21
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N];
+
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			for(int j = a; j <= b; j++) {
+				arr[j - 1] = c;
+			}
+		}
+
+		for(int a : arr) {
+			System.out.print(a + " ");
+		}
+	}
+}
