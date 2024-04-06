be5c1abdc4e1b370ede1e1e4613c017646b4d3e9
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-04-06 23:28:00 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,26 @@
+/*
+ * @since 2024-02-02
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        System.out.println(n + m);
+    }
+}
