9738bcfa5792df1c4b2a3c6cd59506a6d536851a
Nect2r <71915361+nect2r@users.noreply.github.com>
2024-05-20 12:43:40 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
  */
 class Main {
 
-    static int[] items = new int[50];
+    static int[] items = new int[26];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
